6401548768a89510933df019920c6c5ef37e714a
Gordon David Mosher <gdmosher@calgro.com>
2019-12-22 22:27:36 -0800

    Apply BioConductor style

--- R/STARcounts.R
+++ R/STARcounts.R
@@ -18,23 +18,23 @@
 #' countDF <- read.delim("results/STARcountDFeByg.xls", row.names=1, check.names=FALSE)
 #' head(countDF)
 write.table_STARcounts <- function(results_path="./results", table_name="STARcountDFeByg.xls", dbg=0) {
-  details <- file.info(list.files( path = results_path, pattern = "*ReadsPerGene.out.tab$", full.names = TRUE, recursive = TRUE ))
+  details <- file.info(list.files(path=results_path, pattern="*ReadsPerGene.out.tab$", full.names=TRUE, recursive=TRUE))
   details <-  details[with(details, order(as.POSIXct(ctime))), ]
   ff <- rownames(details) # from stackoverflow 13762224, filenames sorted by created time
   if (length(ff) == 0) {
     stop("No STAR count files found in results_path.")
   }
-  counts.files <- lapply( ff, read.table, skip = 4 )
+  counts.files <- lapply(ff, read.table, skip=4)
   ## column 1: gene ID
   ## column 2: counts for unstranded RNA-seq
   ## column 3: counts for the 1st read strand aligned with RNA (htseq-count option -s yes)
   ## column 4: counts for the 2nd read strand aligned with RNA (htseq-count option -s reverse)
-  counts <- as.data.frame( sapply( counts.files, function(x) x[ , 'V2' ] ) ) # V2 is column 2: counts for unstranded RNA-seq
+  counts <- as.data.frame(sapply(counts.files, function(x) x[ , 'V2' ])) # V2 is column 2: counts for unstranded RNA-seq
   ## prepare colnames for xls file
   ff <- gsub( "[.]ReadsPerGene[.]out[.]tab", "", ff ) # get rid of all of the filename except the SampleName
   ff <- gsub( "[.]/results/", "", ff )
-  .last <- function(x) { return( x[length(x)] ) } # or use tail(x,1) since ns slower is ok
-  xxx <- lapply( ff, function(x) { .last(strsplit(x, split="\\/")[[1]]) } ) # keep all that follows last slash, the SampleName
+  .last <- function(x) { return(x[length(x)]) } # or use tail(x,1) since ns slower is ok
+  xxx <- lapply(ff, function(x) { .last(strsplit(x, split="\\/")[[1]]) }) # keep all that follows last slash, the SampleName
   ff <- unlist(xxx)
   colnames(counts) <- ff
   row.names(counts) <- counts.files[[1]]$V1 # V1 are the gene ID's, see STAR Manual --quantMode
