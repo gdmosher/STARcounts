c30b84d737b71533993a3e3785b241d150e98365
Gordon David Mosher <gdmosher@calgro.com>
2019-12-26 17:15:03 -0800

    Add testthat. Roxygen clean up

--- R/STARcounts.R
+++ R/STARcounts.R
@@ -1,7 +1,7 @@
 #' STAR counts
 #'
 #' This function loads STAR Aligner count files *ReadsPerGene.out.tab$ from results_path, accumulates the
-#' data into a data frame, then writes the results to table_name.
+#' data into a data frame, then writes the results in xls format to table_name.
 #' Some code from biostars 241602.
 #' Some code from stackoverflow 13762224.
 #' annotation.gtf must already exist before the mapping step, ./gffread -T annotation.gff -o annotation.gtf
@@ -13,6 +13,7 @@
 #' @return
 #' @export
 #'
+#' @importFrom utils read.table write.table
 #' @examples
 #' write.table_STARcounts(results_path="./results", table_name="STARcountDFeByg.xls")
 #' countDF <- read.delim("results/STARcountDFeByg.xls", row.names=1, check.names=FALSE)
@@ -22,17 +23,18 @@ write.table_STARcounts <- function(results_path="./results", table_name="STARcou
   details <-  details[with(details, order(as.POSIXct(ctime))), ]
   ff <- rownames(details) # from stackoverflow 13762224, filenames sorted by created time
   if (length(ff) == 0) {
-    stop("No STAR count files found in results_path.")
+    stop("No STAR count files found in a recursive search of results_path.")
   }
   counts.files <- lapply(ff, read.table, skip=4)
+  ## see STAR Manual --quantMode
   ## column 1: gene ID
   ## column 2: counts for unstranded RNA-seq
   ## column 3: counts for the 1st read strand aligned with RNA (htseq-count option -s yes)
   ## column 4: counts for the 2nd read strand aligned with RNA (htseq-count option -s reverse)
   counts <- as.data.frame(sapply(counts.files, function(x) x[ , 'V2' ])) # V2 is column 2: counts for unstranded RNA-seq
   ## prepare colnames for xls file
-  ff <- gsub( "[.]ReadsPerGene[.]out[.]tab", "", ff ) # get rid of all of the filename except the SampleName
-  ff <- gsub( "[.]/results/", "", ff )
+  ff <- gsub("[.]ReadsPerGene[.]out[.]tab", "", ff) # get rid of all of the filename except the SampleName
+  ff <- gsub("[.]/results/", "", ff)
   .last <- function(x) { return(x[length(x)]) } # or use tail(x,1) since ns slower is ok
   xxx <- lapply(ff, function(x) { .last(strsplit(x, split="\\/")[[1]]) }) # keep all that follows last slash, the SampleName
   ff <- unlist(xxx)
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(STARcounts)
+
+test_check("STARcounts")
