7467b169f8e262330250b8b58954a1ad877d02b8
Gordon David Mosher <gdmosher@calgro.com>
2020-01-06 14:16:24 -0800

    Clean up man page. Use extdata for example and vignette

--- R/STARcounts.R
+++ R/STARcounts.R
@@ -1,24 +1,30 @@
+#' @title
 #' STAR counts
 #'
-#' This function loads STAR Aligner count files *ReadsPerGene.out.tab$ from results_path, accumulates the
+#' @description
+#' This function loads preexisting STAR Aligner count files *ReadsPerGene.out.tab$ from results_path, accumulates the
 #' data into a data frame, then writes the results in xls format to table_name.
-#' Some code from biostars 241602.
-#' Some code from stackoverflow 13762224.
+#' @details
+#' _SampleName_.ReadsPerGene.out.tab must already exist (created during mapping step) \cr
 #' annotation.gtf must already exist before the mapping step, ./gffread -T annotation.gff -o annotation.gtf
-#' _SampleName_.ReadsPerGene.out.tab must already exist (created during mapping step)
 #' @param results_path Path to the STAR Aligner count files
 #' @param table_name Name of the output file including the .xls extension
 #' @param dbg Optional debug level defaults to 0
 #'
-#' @return
+#' @return NULL
 #' @export
 #'
 #' @importFrom utils read.table write.table
 #' @examples
-#' write.table_STARcounts(results_path="./results", table_name="STARcountDFeByg.xls")
-#' countDF <- read.delim("results/STARcountDFeByg.xls", row.names=1, check.names=FALSE)
+#' results_path <- system.file("extdata", "results/", package = "STARcounts")
+#' write.table_STARcounts(results_path=results_path, table_name="STARcountDFeByg.xls")
+#' countDF <- read.delim("STARcountDFeByg.xls", row.names=1, check.names=FALSE)
 #' head(countDF)
 write.table_STARcounts <- function(results_path="./results", table_name="STARcountDFeByg.xls", dbg=0) {
+  ## Some code from biostars 241602.
+  ## Some code from stackoverflow 13762224.
+  ## annotation.gtf must already exist before the mapping step, ./gffread -T annotation.gff -o annotation.gtf
+  ## _SampleName_.ReadsPerGene.out.tab must already exist (created during mapping step)
   details <- file.info(list.files(path=results_path, pattern="*ReadsPerGene.out.tab$", full.names=TRUE, recursive=TRUE))
   details <-  details[with(details, order(as.POSIXct(ctime))), ]
   ff <- rownames(details) # from stackoverflow 13762224, filenames sorted by created time
@@ -40,7 +46,8 @@ write.table_STARcounts <- function(results_path="./results", table_name="STARcou
   ff <- unlist(xxx)
   colnames(counts) <- ff
   row.names(counts) <- counts.files[[1]]$V1 # V1 are the gene ID's, see STAR Manual --quantMode
-  write.table(counts, paste0(results_path, "/", table_name), col.names=NA, quote=FALSE, sep="\t")
+  write.table(counts, paste0(table_name), col.names=NA, quote=FALSE, sep="\t")
+  # write.table(counts, paste0(results_path, "/", table_name), col.names=NA, quote=FALSE, sep="\t")
 }
 
 
